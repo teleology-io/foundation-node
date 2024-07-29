7e2549c858d53a7236d03e29c853a79552e7ba55
Chris Sullivan <chrissullivan.dev@gmail.com>
2024-07-28 21:00:42 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# foundation-node
-Node.js Library for the Foundation API
+# Foundation Library
+The Foundation Node & browser Library is your gateway to effortless interaction with the Foundation API. Designed with simplicity and efficiency in mind, this library abstracts away the complexity of direct API calls, providing a clean and intuitive interface for developers.
+
+## Installation
+
+```
+npm -i @teleology/foundation
+```
+
+or 
+
+```
+yarn add @teleology/foundation
+```
+
+## Requirements
+- axios ^1.7.2
+- websocket ^1.0.35
+
+## Usage Example:
+```javascript
+const { Foundation } = require('@teleology/foundation-sdk');
+
+(async () => {
+    const sdk = new Foundation({
+        url: 'https://foundation-api.teleology.io',
+        apiKey: '<your-api-key>',
+        uid: '<user-unique-id>'
+    })
+
+    sdk.subscribe(console.log)
+
+
+    console.log('Env is', await sdk.getEnvironment())
+    console.log("config", await sdk.getConfiguration())
+    console.log("variable is", await sdk.getVariable('numb', 20))
+})();
+```
---
+++ src/index.ts
@@ -0,0 +1,140 @@
+import axios from 'axios';
+import { w3cwebsocket as Websocket } from 'websocket';
+
+interface ResolvedVariable {
+  id: string
+  name: string
+  value: any
+}
+
+interface VariableCache {
+  [index: string]: ResolvedVariable
+}
+
+export function Foundation({ url, apiKey, uid }: {
+  url: string
+  apiKey: string
+  uid?: string
+}) {
+  let config: object | undefined
+  let environment: object | undefined
+  let variables: VariableCache = {}
+  let callback: (event?: string, data?: any) => void
+
+  const socketUrl = `${url}/v1/realtime?apiKey=${apiKey}`.replace("http", "ws")
+  const client = axios.create({
+    baseURL: url,
+    headers: {
+      ['X-Api-Key']: apiKey
+    },
+  })
+
+  client.interceptors.response.use((res) => res.data, (err) => undefined)
+
+  async function getEnvironment() {
+    if (!environment) {
+      environment = await client({
+        url: '/v1/environment',
+        method: 'get'
+      })
+    }
+
+    return environment
+  }
+
+  async function getConfiguration() {
+    if (!config) {
+      const result: any = await client({
+        url: '/v1/configuration',
+        method: 'get'
+      })
+
+      if (result.mime_type === 'application/json') {
+        config = JSON.parse(result.content)
+      } else {
+        config = result.content
+      }
+    }
+
+    return config
+  }
+
+  async function getVariable(name: string, fallback?: any) {
+    const found = variables[name]
+    if (found) {
+      return found.value
+    }
+
+    const result: ResolvedVariable = await client({
+      url: '/v1/variable',
+      method: 'post',
+      data: {
+        name,
+        uid,
+      }
+    })
+    if (result) {
+      variables[name] = result;
+      return result.value
+    }
+
+    return fallback
+  }
+
+  function subscribe(cb: (event?: string, data?: any) => void) {
+    callback = cb
+  }
+
+  function realtime(callback: (data?: any) => void) {
+    const ws = new Websocket(socketUrl)
+
+    ws.onerror = () => ws.close()
+
+    ws.onclose = () => {
+      setTimeout(() => realtime(callback), 1000)
+    }
+
+    ws.onmessage = function (e) {
+      if (typeof e.data === 'string') {
+        try {
+          callback(JSON.parse(e?.data))
+        } catch (err) {
+          callback(e.data)
+        }
+      }
+    }
+  }
+
+  realtime(async (data: any) => {
+    switch (data?.type) {
+      case 'variable.updated': {
+        const name = data.payload.name
+
+        delete variables[name];
+        await getVariable(data?.payload?.name)
+
+        callback && callback(data.type, variables[name])
+        break;
+      }
+      case 'configuration.published': {
+        config = undefined
+
+        callback && callback(data.type, await getConfiguration())
+        break;
+      }
+      case 'environment.updated': {
+        environment = undefined;
+
+        callback && callback(data.type, await getEnvironment())
+        break;
+      }
+    }
+  })
+
+  return {
+    getConfiguration,
+    getEnvironment,
+    getVariable,
+    subscribe,
+  }
+}
